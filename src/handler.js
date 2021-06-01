d74cd13fbbb835dff9035f6a55aeb608a4db9e67
dian-tri-budianto <dian.budianto@berca.co.id>
2021-06-01 21:39:09 +0700

    implement route and handler for using post book
    update eslint rules in object-curly-newline

--- src/handler.js
+++ src/handler.js
@@ -1,1 +1,78 @@
 // handler.js : Memuat seluruh fungsi-fungsi handler yang digunakan pada berkas routes.
+
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBooksHandler = (req, h) => {
+  const { name, year, author, summary, publisher, pageCount, readPage = 0, reading = false } = req.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = false;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  let status = '';
+  let message = '';
+  let data = '';
+  let codeResponse = '';
+
+  if (name) {
+    if (readPage < pageCount) {
+      if (isSuccess) {
+        status = 'success';
+        message = 'Buku berhasil ditambahkan';
+        data = {
+          bookId: newBook,
+        };
+        codeResponse = 201;
+      } else {
+        status = 'error';
+        message = 'Buku gagal ditambahkan';
+        data = {};
+        codeResponse = 500;
+      }
+    } else {
+      status = 'fail';
+      message = 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount';
+      data = {};
+      codeResponse = 400;
+    }
+  } else {
+    status = 'fail';
+    message = 'Gagal menambahkan buku. Mohon isi nama buku';
+    data = {};
+    codeResponse = 400;
+  }
+
+  const response = h.response({
+    status: status.toString(),
+    message: message.toString(),
+    data,
+  });
+
+  response.code(codeResponse);
+  return response;
+};
+
+module.exports = {
+  addBooksHandler,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 // routes.js : Memuat kode konfigurasi routing server seperti menentukan path, method, dan handler yang digunakan.
 
+const { addBooksHandler } = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/books',
-    handler: () => {},
+    handler: addBooksHandler,
   },
   {
     method: 'GET',
